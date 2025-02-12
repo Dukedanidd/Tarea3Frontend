286595f44035da3429758202e949e7f20d1cd565
Jasiel Corral <jasho77712345@gmail.com>
2025-02-11 23:30:29 -0600

    Create main.js and add calculator class

---
+++ main.js
@@ -0,0 +1,9 @@
+class Calculator {
+  constructor() {
+    this.display = document.getElementById("display")
+    this.currentValue = ""
+    this.previousValue = ""
+    this.operation = null
+    this.shouldResetDisplay = false
+  }
+}
